341d4067e3967d8f6bd78fd53b189dca7eb3889b
ICI <ICI@DESKTOP-EA5M9P6>
2023-06-21 11:46:34 +0900

    commit01

---
+++ src/main/java/com/gyojincompany/board/Gittestpro20230621Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittestpro20230621Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gittestpro20230621Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/board/Gittestpro20230621ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gittestpro20230621ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
